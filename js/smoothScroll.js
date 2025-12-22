288b2d8dfdbef0ce4af2fc6a8ed0a2115451106b
Zaki Ahmed <reach.zaki.ahmed@gmail.com>
2025-12-21 23:16:12 -0600

    Updated index.html

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,1 +1,10 @@
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
+
+ScrollSmoother.create({
+  wrapper: '#smooth-wrapper',
+  content: '#smooth-content',
+  smooth: 2,
+  effects: true,
+  smoothTouch: 0.1,
+  normalizeScroll: true
+});
