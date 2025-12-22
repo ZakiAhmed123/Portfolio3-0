f633105ca498ff7a78da57bc0ce0d4547c3771b9
Zaki Ahmed <reach.zaki.ahmed@gmail.com>
2025-12-21 21:34:28 -0600

    Updated nethealth.html

---
+++ js/scrollingText.js
@@ -0,0 +1,35 @@
+gsap.registerPlugin(ScrollTrigger);
+
+window.addEventListener('load', () => {
+  const textItems = document.querySelectorAll('.scroll-text-item');
+
+  textItems.forEach((item, index) => {
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: item,
+        start: 'top center',
+        end: 'bottom center',
+        scrub: 1,
+        onUpdate: (self) => {
+          const progress = self.progress;
+
+          if (progress < 0.5) {
+            const opacity = 0.3 + (progress * 2) * 0.7;
+            gsap.to(item, {
+              opacity: opacity,
+              duration: 0.3,
+              ease: 'none'
+            });
+          } else {
+            const opacity = 1 - ((progress - 0.5) * 2) * 0.7;
+            gsap.to(item, {
+              opacity: opacity,
+              duration: 0.3,
+              ease: 'none'
+            });
+          }
+        }
+      }
+    });
+  });
+});
