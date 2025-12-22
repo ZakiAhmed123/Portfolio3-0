db29540b1acc9459c9180c1e6417c95a31daf36a
Zaki Ahmed <reach.zaki.ahmed@gmail.com>
2025-12-21 22:46:24 -0600

    Updated main.css

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -3,7 +3,10 @@ gsap.registerPlugin(ScrollSmoother);
 ScrollSmoother.create({
   wrapper: '#smooth-wrapper',
   content: '#smooth-content',
-  smooth: 2,
+  smooth: 2.5,
   effects: true,
-  smoothTouch: 0.1
+  smoothTouch: 0.1,
+  speed: 0.7,
+  normalizeScroll: true,
+  ease: 'power2.out'
 });
