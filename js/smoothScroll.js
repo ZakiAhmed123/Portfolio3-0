741898197f730cd75b159ea5aa653a6556bc5978
Zaki Ahmed <reach.zaki.ahmed@gmail.com>
2025-12-21 23:17:31 -0600

    Updated smoothScroll.js

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,10 +1,12 @@
-gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SlowMo);
 
-ScrollSmoother.create({
-  wrapper: '#smooth-wrapper',
-  content: '#smooth-content',
-  smooth: 2,
-  effects: true,
-  smoothTouch: 0.1,
-  normalizeScroll: true
+  ScrollSmoother.create({
+    wrapper: '#smooth-wrapper',
+    content: '#smooth-content',
+    smooth: 2,
+    effects: true,
+    smoothTouch: 0.1,
+    normalizeScroll: true
+  });
 });
