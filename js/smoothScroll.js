991225fb8b49469449682cc02ccdeceee873bd9b
Zaki Ahmed <reach.zaki.ahmed@gmail.com>
2025-12-21 22:53:51 -0600

    Updated smoothScroll.js

--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -3,11 +3,8 @@ gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
 let smoother = ScrollSmoother.create({
   wrapper: '#smooth-wrapper',
   content: '#smooth-content',
-  smooth: 2.5,
+  smooth: 1,
   effects: true,
   smoothTouch: 0.1,
   normalizeScroll: true
 });
-
-// Refresh ScrollTrigger after smoother is created
-ScrollTrigger.refresh();
