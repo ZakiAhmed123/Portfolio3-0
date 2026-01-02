02e48088e030adf70e634735e31079574b08b5ff
Zaki Ahmed <reach.zaki.ahmed@gmail.com>
2026-01-01 23:29:13 -0600

    Updated nethealth.html

---
+++ js/squaresParallax.js
@@ -0,0 +1,42 @@
+import { gsap } from './GSAP/index.js';
+import { ScrollTrigger } from './GSAP/scrolltrigger.js';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const square1 = document.getElementById('square-1');
+const square2 = document.getElementById('square-2');
+const square3 = document.getElementById('square-3');
+const square4 = document.getElementById('square-4');
+
+gsap.to(square2, {
+  y: 50,
+  ease: 'none',
+  scrollTrigger: {
+    trigger: square2,
+    start: 'top bottom',
+    end: 'bottom top',
+    scrub: true
+  }
+});
+
+gsap.to(square3, {
+  y: 100,
+  ease: 'none',
+  scrollTrigger: {
+    trigger: square3,
+    start: 'top bottom',
+    end: 'bottom top',
+    scrub: true
+  }
+});
+
+gsap.to(square4, {
+  y: 150,
+  ease: 'none',
+  scrollTrigger: {
+    trigger: square4,
+    start: 'top bottom',
+    end: 'bottom top',
+    scrub: true
+  }
+});
