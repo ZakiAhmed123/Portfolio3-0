3bbdcadf7272d6d4eae7854890d45198249d148d
Zaki Ahmed <reach.zaki.ahmed@gmail.com>
2026-02-24 06:40:46 -0600

    Updated nethealth.html

--- js/squaresParallax.js
+++ js/squaresParallax.js
@@ -3,40 +3,41 @@ import { ScrollTrigger } from './GSAP/scrolltrigger.js';
 
 gsap.registerPlugin(ScrollTrigger);
 
-const square1 = document.getElementById('square-1');
-const square2 = document.getElementById('square-2');
-const square3 = document.getElementById('square-3');
-const square4 = document.getElementById('square-4');
+if (window.innerWidth > 768) {
+  const square2 = document.getElementById('square-2');
+  const square3 = document.getElementById('square-3');
+  const square4 = document.getElementById('square-4');
 
-gsap.to(square2, {
-  y: 100,
-  ease: 'none',
-  scrollTrigger: {
-    trigger: square2,
-    start: 'top bottom',
-    end: 'bottom top',
-    scrub: true
-  }
-});
+  gsap.to(square2, {
+    y: 100,
+    ease: 'none',
+    scrollTrigger: {
+      trigger: square2,
+      start: 'top bottom',
+      end: 'bottom top',
+      scrub: true
+    }
+  });
 
-gsap.to(square3, {
-  y: 200,
-  ease: 'none',
-  scrollTrigger: {
-    trigger: square3,
-    start: 'top bottom',
-    end: 'bottom top',
-    scrub: true
-  }
-});
+  gsap.to(square3, {
+    y: 200,
+    ease: 'none',
+    scrollTrigger: {
+      trigger: square3,
+      start: 'top bottom',
+      end: 'bottom top',
+      scrub: true
+    }
+  });
 
-gsap.to(square4, {
-  y: 300,
-  ease: 'none',
-  scrollTrigger: {
-    trigger: square4,
-    start: 'top bottom',
-    end: 'bottom top',
-    scrub: true
-  }
-});
+  gsap.to(square4, {
+    y: 300,
+    ease: 'none',
+    scrollTrigger: {
+      trigger: square4,
+      start: 'top bottom',
+      end: 'bottom top',
+      scrub: true
+    }
+  });
+}
